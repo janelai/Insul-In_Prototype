f6fdaa6f558ae3e6ed6b7a375c0c347c1330be00
OceanaLee <lee.oceana001@gmail.com>
2023-05-04 12:41:35 -0700

    Cell Walls Working as Bricks
    Fixed the script for the brick and renamed it to be BrickBehavior.cs
    
    Bricks will now be destroyed when collided with (we'll need to modify this later to check that it is colliding with an insulin ball specifically) and bounces back whatever collides with it

---
+++ Insul-In Prototype/Assets/Scripts/BrickBehavior.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrickBehavior : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        this.gameObject.SetActive(false);
+        //Debug.Log("Box hit");
+    }
+}
